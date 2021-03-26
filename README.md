c5e1e98552fd5adcca956e984e65b6913aa72e83
Itzbenz <gamerrindoubuntu@gmail.com>
2021-03-26 09:53:45 +0100

    mmm

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### NSFW API Detection with nodejs
 Adopted from:
-https://github.com/SashiDo/content-moderation-image-api
+https://github.com/SashiDo/content-moderation-image-api, https://github.com/infinitered/nsfwjs
--- src/NSFWModel.js
+++ src/NSFWModel.js
@@ -2,12 +2,25 @@
 const axios = require( "axios");
 const tf =  require( "@tensorflow/tfjs-node");
 const nsfw = require( "nsfwjs");
-
+const jpeg = require('jpeg-js')
 tf.enableProdMode(); // enable on production
 
 let model
 
+const convert = async (img) => {
+    // Decoded image in UInt8 Byte array
+    const image = await jpeg.decode(img, true)
 
+    const numChannels = 3
+    const numPixels = image.width * image.height
+    const values = new Int32Array(numPixels * numChannels)
+
+    for (let i = 0; i < numPixels; i++)
+        for (let c = 0; c < numChannels; ++c)
+            values[i * numChannels + c] = image.data[i * 4 + c]
+
+    return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
+}
 module.exports = {
     init: async function(){
         const model_url = process.env.NSFW_MODEL_URL | "https://ml.files-sashido.cloud/models/nsfw_mobilenet_v2/93/";
