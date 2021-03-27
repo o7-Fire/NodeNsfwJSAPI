545f214feaa73c8c7d2f9bd17d069bbcecff4f42
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-03-27 02:02:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,172 @@
-// server.js
-// where your node app starts
+// you can use any other http client
+const axios = require("axios");
+const tf = require("@tensorflow/tfjs-node");
+const nsfw = require("nsfwjs");
+const jpeg = require("jpeg-js");
+tf.enableProdMode(); // enable on production
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const nsfwModel = require("./src/NSFWModel");
-
-nsfwModel.init();
-// make all the files in 'public' available
-app.use(express.static("public"));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-
-app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/views/index.html");
-});
-let cache = [];
-let discordVideo = [".gif", ".mp4", ".webm"];
-
-async function classify(url, req, res) {
-    console.log(req.url + ":" + url);
-    const hash = url;
-
-    try {
-        if (!cache[hash]) {
-            cache[hash] = await nsfwModel.classify(url);
-        }
-        res.json(cache[hash]);
-    } catch (err) {
-        res.status(500);
-        res.send("wtf");
-        console.log(err);
-    }
+let model;
+let currentModel = {
+    url : "Default",
+    size : "Default"
 }
-app.get("/api/json/graphical", (req, res) => {
-    res.json(nsfwModel.list)
-})
-app.get("/api/json/graphical/classification/*", async (req, res) => {
-    let url = req.url.replace("/api/json/graphical/classification/", "");
-    if (!url) return
-    let body = {}
-    let allowed = true;
-    body.error = "Not allowed"
-    res.status(405);
-    if (!url.startsWith("https://cdn.discordapp.com/")) {
-        res.status(415);
-        body.error = "Only allow https://cdn.discordapp.com/"
-        res.json(body);
-        allowed = false;
+//Using n1 do 1 - (n1 - n2)
+const report = {
+    Drawing: {
+        Hentai: "Anime",
+        Sexy: "ArtificialProvocative",
+        Neutral: "DigitalDrawing"
+    },
+    Neutral: {
+        Drawing: "Digital" ,
+        Sexy:  "NaturallyProvocative" ,
+        Porn:  "Disturbing" ,
+        Hentai:  "SeductiveArt"
+    },
+    Sexy: {
+        Neutral: "SexuallyProvocative",
+        Porn:  "SeductivePorn"
+    },
+    Porn: {
+        Sexy: "PornSeductive",
+        Hentai:  {n1: "HentaiClips"},
+        Neutral: "SoftPorn"
+    },
+    Hentai: {
+        Porn: "Doujin",
+        Drawing:  "R34"
     }
-    if (allowed)
-        for (const discordVideoKey in discordVideo) {
-            if (url.endsWith(discordVideo[discordVideoKey])) {
-                allowed = false;
-                if (url.startsWith("https://cdn.discordapp.com/")) {
-                    url = url + "?format=png";
-                    url = url.replace(
-                        "https://cdn.discordapp.com/",
-                        "https://media.discordapp.net/"
-                    );
-                    allowed = true;
-                    break;
+}; //Combining thing make new thing
+const convert = async img => {
+    // Decoded image in UInt8 Byte array
+    const image = await jpeg.decode(img, true);
+
+    const numChannels = 3;
+    const numPixels = image.width * image.height;
+    const values = new Int32Array(numPixels * numChannels);
+
+    for (let i = 0; i < numPixels; i++)
+        for (let c = 0; c < numChannels; ++c)
+            values[i * numChannels + c] = image.data[i * 4 + c];
+
+    return tf.tensor3d(values, [image.height, image.width, numChannels], "int32");
+};
+
+function assignReport(t1, t2, reportPrediction) {
+    let v2;
+    let c2;
+    let v1;
+    let c1;
+    c1 = t1.className;
+    v1 = t1.probability;
+    c2 = t2.className;
+    v2 = t2.probability;
+    if (report[c1][c2]) {
+        let c3 = report[c1][c2];
+        if (c3.n1) {
+            reportPrediction[c3.n1] = v1 - v2;
+            reportPrediction[c3.n1] = 1 - reportPrediction[c3.n1];
+        } else {
+            reportPrediction[c3] = v1 - v2;
+        }
+    } else {
+        console.log("Not implemented: " + t1.className + ":" + t2.className);
+    }
+    return reportPrediction
+}
+module.exports = {
+    init: async function() {
+        const model_url = process.env.NSFW_MODEL_URL;
+        const shape_size = process.env.NSFW_MODEL_SHAPE_SIZE;
+
+        // Load the model in the memory only once!
+        if (!model) {
+            try {
+                //model_url, { size: parseInt(shape_size) }
+                if (!model_url || !shape_size) model = await nsfw.load();
+                else {
+                    model = await nsfw.load(model_url, { size: parseInt(shape_size) });
+                    currentModel.size = shape_size
+                    currentModel.url = model_url
+                    console.info("Loaded: " + model_url + ":" + shape_size);
                 }
+                console.info("The NSFW Model was loaded successfully!");
+            } catch (err) {
+                console.error(err);
             }
         }
-    if (!allowed) {
-        res.json(body);
-        return;
-    }
-    await classify(url, req, res);
-});
+    },
+    classify: async function(url) {
+        let pic;
+        let result = {};
 
-app.post("/api/*", (request, response) => {
-    console.dir(request.body);
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("404");
-});
-// listen for requests :)
-const listener = app.listen(process.env.PORT || 5656, () => {
-    console.log("Your app is listening on port " + listener.address().port);
-});
+        try {
+            pic = await axios.get(url, {
+                responseType: "arraybuffer"
+            });
+        } catch (err) {
+            console.error("Download Image Error:", err);
+            result.error = err;
+            return result;
+        }
+
+        try {
+            // Image must be in tf.tensor3d format
+            // you can convert image to tf.tensor3d with tf.node.decodeImage(Uint8Array,channels)
+            const image = await tf.node.decodeImage(pic.data, 3);
+            let prediction;
+            if (url.toString().endsWith(".gif")) {
+                prediction = await model.classifyGif(image);
+            } else {
+                prediction = await model.classify(image);
+            }
+            let reportPrediction = {};
+            let t1 = prediction[0];
+            let t2 = prediction[1];
+            let t3 = prediction[2];
+            let t4 = prediction[4];
+            reportPrediction = assignReport(t1, t2, reportPrediction);
+            //reportPrediction = assignReport(t3, t4, reportPrediction);
+            image.dispose(); // Tensor memory must be managed explicitly (it is not sufficient to let a tf.Tensor go out of scope for its memory to be released).
+            for (const predictionKey1 in prediction) {
+                let c1 = prediction[predictionKey1].className;
+                reportPrediction[c1] = prediction[predictionKey1].probability;
+            }
+            /*
+                  for (const predictionKey1 in prediction) {
+                      let c1 = prediction[predictionKey1].className;
+                      let v1 = prediction[predictionKey1].probability;
+                      for (const predictionKey2 in prediction) {
+
+                          let c2 = prediction[predictionKey2].className;
+                          let v2 = prediction[predictionKey2].probability;
+                          c1 = t1.className
+                          v1 = t1.probability
+                          c2 = t2.className
+                          v2 = t2.probability
+                          let c3 = report[c1][c2];
+                          if (!c3) continue;
+                          if(c3.n1) {
+                              reportPrediction[c3.n1] = v1 - v2;
+                              reportPrediction[c3.n1] = 1 - reportPrediction[c3.n1]
+                          }else {
+                              reportPrediction[c3] =  v1 - v2;
+                          }
+                      }
+                  }
+
+                   */
+            //reportPrediction["Goodluck Figuring out this data"] = 0;
+            //reportPrediction["ModelURL "+  currentModel.name] = 0
+            //reportPrediction["ModelSize "+  currentModel.size] = 0
+            reportPrediction.model = currentModel
+            result = reportPrediction;
+        } catch (err) {
+            console.error("Prediction Error: ", err);
+            result.error = "Model is not loaded yet!";
+            return result;
+        }
+
+        return result;
+    }
+};
