9758803cdd8838fe1a10eb6b506b21b41c77b30a
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 11:39:03 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 const assad = require("./index.js");
 const axios = require('axios');
 async function test1() {
-  try {
-    const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png');
-    console.log(response);
-     process.exit(0);
-  } catch (error) {
-    console.error(error);
-     process.exit(1);
-  }
+    try {
+        const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png');
+        console.log(response.data);
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
 
 
 test1();
-
