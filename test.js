b17e66723570a7c3d12a43871845e714641540b4
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 11:46:41 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ async function test1() {
     try {
         const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png');
         console.log(response.data);
+        JSON.parse(response.data);
         process.exit(0);
     } catch (error) {
         console.error(error);
