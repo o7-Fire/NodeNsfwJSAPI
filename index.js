954e632354bab75df9a700e89b199c18a717c09a
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 12:58:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,107 +8,119 @@ const app = express();
 const bodyParser = require("body-parser");
 const nsfwModel = require("./src/NSFWModel");
 nsfwModel.init().then(() => {
-  cache = [];
+    cache = [];
 });
 
 app.head("/", (request, response) => {
-  response.status(200);
+    response.status(200);
 });
 // make all the files in 'public' available
 app.use(express.static("public"));
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 app.use(function(req, res, next) {
-  if (!req.headers.authorization && process.env.SECRET) {
-    console.log("no auth");
-    return res.status(403).json({ error: "No credentials sent!" });
-  } else if (req.headers.authorization !== process.env.SECRET) {
-    console.log("invalid auth");
-    return res.status(403).json({ error: "No credentials sent!!" });
-  } else {
-    next();
-  }
+    if (!req.headers.authorization && process.env.SECRET) {
+        console.log("no auth");
+        return res.status(403).json({
+            error: "No credentials sent!"
+        });
+    } else if (req.headers.authorization !== process.env.SECRET) {
+        console.log("invalid auth");
+        return res.status(403).json({
+            error: "No credentials sent!!"
+        });
+    } else {
+        next();
+    }
 });
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+    response.sendFile(__dirname + "/views/index.html");
 });
 let cache = [];
 let discordVideo = [".gif", ".mp4", ".webm"];
 
 async function classify(url, req, res) {
-  console.log(req.url + ":" + url);
-  const hash = url;
+    console.log(req.url + ":" + url);
+    const hash = url;
 
-  try {
-    if (!cache[hash]) {
-      cache[hash] = await nsfwModel.classify(url);
+    try {
+        if (!cache[hash]) {
+            cache[hash] = await nsfwModel.classify(url);
+        }
+        res.json(cache[hash]);
+    } catch (err) {
+        res.status(500);
+        res.send("wtf");
+        console.log(err);
     }
-    res.json(cache[hash]);
-  } catch (err) {
-    res.status(500);
-    res.send("wtf");
-    console.log(err);
-  }
 }
 app.get("/api/json/test", (req, res) => {
-  res.send(req.body);
+    res.send(req.body);
 });
 app.get("/api/json/graphical", (req, res) => {
-  res.json(nsfwModel.report);
+    res.json(nsfwModel.report);
 });
 app.get("/api/json/graphical/classification/*", async (req, res) => {
-  let url = req.url.replace("/api/json/graphical/classification/", "");
-  if (!url) return;
-  let body = {};
-  let allowed = true;
-  body.error = "Not allowed";
-  status = 405;
-  if (!url.startsWith("https://cdn.discordapp.com/")) {
-    if (
-      !(url.endsWith(".png") || url.endsWith(".jpeg") || url.endsWith(".bmg") || url.endsWith(".jpg"))
-    ) {
-      res.status(415);
-      body.error = "Only allow https://cdn.discordapp.com/ or picture";
-      res.json(body);
-      allowed = false;
-    }
-  } else {
-    for (const discordVideoKey in discordVideo) {
-      if (url.endsWith(discordVideo[discordVideoKey])) {
-        allowed = false;
-        if (url.startsWith("https://cdn.discordapp.com/")) {
-          url = url + "?format=png";
-          url = url.replace(
-            "https://cdn.discordapp.com/",
-            "https://media.discordapp.net/"
-          );
-          allowed = true;
-          break;
+    let url = req.url.replace("/api/json/graphical/classification/", "");
+    if (!url) return;
+    let body = {};
+    let allowed = true;
+    body.error = "Not allowed";
+    status = 405;
+    if (url.startsWith("https://cdn.discordapp.com/") || url.startsWith("https://media.discordapp.net/")) {
+        for (const discordVideoKey in discordVideo) {
+            if (url.endsWith(discordVideo[discordVideoKey])) {
+                allowed = false;
+                if (url.startsWith("https://cdn.discordapp.com/")) {
+                    url = url + "?format=png";
+                    url = url.replace(
+                        "https://cdn.discordapp.com/",
+                        "https://media.discordapp.net/"
+                    );
+                    allowed = true;
+                    break;
+                }
+            }
         }
-      }
-    }
-  }
 
-  if (!allowed) {
-    res.status(status).json(body);
-    return;
-  }
-  await classify(url, req, res);
+
+    } else {
+
+        if (
+            !(url.endsWith(".png") || url.endsWith(".jpeg") || url.endsWith(".bmg") || url.endsWith(".jpg"))
+        ) {
+            res.status(415);
+            body.error = "Only allow https://cdn.discordapp.com/ or picture";
+            res.json(body);
+            allowed = false;
+        }
+
+    }
+
+    if (!allowed) {
+        res.status(status).json(body);
+        return;
+    }
+    await classify(url, req, res);
 });
 
 app.get("*", function(req, res) {
-  res.status(404);
+    res.status(404);
 
-  // respond with json
-  if (req.accepts("json")) {
-    res.json({ error: "Not found" });
-    return;
-  }
+    // respond with json
+    if (req.accepts("json")) {
+        res.json({
+            error: "Not found"
+        });
+        return;
+    }
 
-  // default to plain-text. send()
-  res.type("txt").send("Not found");
+    // default to plain-text. send()
+    res.type("txt").send("Not found");
 });
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 5656, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
