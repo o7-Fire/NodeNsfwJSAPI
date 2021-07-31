8f4706a8a39bbe4385c25c73887140e46bce6cb7
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 12:11:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 async function test4() {
     try {
+        console.log("test 4");
         const response = await axios.get('http://localhost:5656/api/json/test');
         console.log(response.data);
          process.exit(0);
@@ -13,6 +14,7 @@ async function test4() {
 }
 async function test3() {
     try {
+        console.log("test 3");
         const response = await axios.get('http://localhost:5656/api/json/graphical');
         console.log(response.data);
         JSON.parse(response.data);
@@ -24,6 +26,7 @@ async function test3() {
 }
 async function test2() {
     try {
+        console.log("test 2");
         const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://github.com/o7-Fire/General/raw/master/AI/Logo/Accomplish-o7.png');
         console.log(response.data);
         JSON.parse(response.data);
@@ -35,6 +38,7 @@ async function test2() {
 }
 async function test1() {
     try {
+        console.log("test 1");
         const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png');
         console.log(response.data);
         JSON.parse(response.data);
