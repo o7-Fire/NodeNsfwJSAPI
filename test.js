cffd7de1dd035436cbfe7d030180a2882a493986
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 11:35:30 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,28 +1,14 @@
 const assad = require("./index.js");
-const https = require('https')
-
-async function test1(){
-  const options = {
-  hostname: 'http://localhost',
-  port: 5656,
-  path: '/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png',
-  method: 'GET'
-}
-  const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-
-  res.on('data', d => {
-    process.stdout.write(d)
-    process.exit(0);
-  })
-})
-
-req.on('error', error => {
-  console.error(error);
-  process.exit(1);
-})
-
-req.end()
+const axios = require('axios');
+async function test1() {
+  try {
+    const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png');
+    console.log(response);
+     process.exit(0);
+  } catch (error) {
+    console.error(error);
+     process.exit(1);
+  }
 }
 
 
