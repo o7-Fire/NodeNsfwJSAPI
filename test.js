29998b0f480d5af2ad40422117ed79fc3179bcc3
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 12:23:07 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 async function test4() {
     try {
+        console.log("------");
         console.log("test 4");
         const response = await axios.get('http://localhost:5656/api/json/test');
         console.log(response.data);
@@ -14,13 +15,13 @@ async function test4() {
 }
 async function test3() {
     try {
+        console.log("------");
         console.log("test 3");
         const response = await axios.get('http://localhost:5656/api/json/graphical');
         const data = response.data;
         console.log("```js");
         console.log(data);
         console.log("```");
-        JSON.parse(data);
         test4();
     } catch (error) {
         console.error(error);
@@ -29,6 +30,7 @@ async function test3() {
 }
 async function test2() {
     try {
+        console.log("------");
         console.log("test 2");
         const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://github.com/o7-Fire/General/raw/master/AI/Logo/Accomplish-o7.png');
         const data = response.data;
@@ -44,6 +46,7 @@ async function test2() {
 }
 async function test1() {
     try {
+        console.log("------");
         console.log("test 1");
         const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://media.discordapp.net/attachments/840041811384860709/870629782772150322/TraptrixArchetype.png');
         const data = response.data;
