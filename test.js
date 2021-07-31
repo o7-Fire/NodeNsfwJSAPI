c93e6439989e2911e7ab84f39136d9ec6107c2fd
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-31 12:37:06 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const nsfwModel = require("./src/NSFWModel");
 const axios = require('axios');
-
+const scanList = [
+    "https://cdn.discordapp.com/attachments/840041811384860708/870977097651331072/IMG_20210731_144040.jpg",
+    "https://github.com/o7-Fire/General/raw/master/AI/Logo/Accomplish-o7.png",
+    "https://media.discordapp.net/attachments/840041811384860708/869557735585362001/cancer-memri.gif"
+    ];
 async function test4() {
     try {
         console.log("\n\n");
@@ -29,20 +33,24 @@ async function test3() {
     }
 }
 async function test2() {
+    console.log("\n\n");
+    console.log("Test 2");
+    for(const url of scanList){
     try {
-        console.log("\n\n");
-        console.log("Test 2");
-        const response = await axios.get('http://localhost:5656/api/json/graphical/classification/https://github.com/o7-Fire/General/raw/master/AI/Logo/Accomplish-o7.png');
+ 
+        const response = await axios.get('http://localhost:5656/api/json/graphical/classification/'+url);
         const data = response.data;
         console.log("```js");
+        console.log("Source: " + url);
         console.log(data);
         console.log("```");
-        console.log(data.model);
-        test3();
+       
     } catch (error) {
         console.error(error);
         process.exit(1);
     }
+    }
+     test3();
 }
 async function test1() {
     try {
