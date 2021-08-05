2c0ae6120353c18d7c82a2a23be8e73b6032c034
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-05 16:47:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,14 +4,14 @@ const scanList = [
     "https://cdn.discordapp.com/attachments/840041811384860708/870977097651331072/IMG_20210731_144040.jpg",
     "https://github.com/o7-Fire/General/raw/master/AI/Logo/Accomplish-o7.png",
     "https://media.discordapp.net/attachments/840041811384860708/869557735585362001/cancer-memri.gif"
-    ];
+];
 async function test4() {
     try {
         console.log("\n\n");
         console.log("Test 4");
         const response = await axios.get('http://localhost:5656/api/json/test');
         console.log(response.data);
-         process.exit(0);
+        process.exit(0);
     } catch (error) {
         console.error(error);
         process.exit(1);
@@ -35,22 +35,37 @@ async function test3() {
 async function test2() {
     console.log("\n\n");
     console.log("Test 2");
-    for(const url of scanList){
-    try {
- 
-        const response = await axios.get('http://localhost:5656/api/json/graphical/classification/'+url);
-        const data = response.data;
-        console.log("```js");
-        console.log("Source: " + url);
-        console.log(data);
-        console.log("```");
-       
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
+    for (const url of scanList) {
+        //http
+        try {
+
+            const response = await axios.get('http://localhost:5656/api/json/graphical/classification/' + url);
+            const data = response.data;
+            console.log("```js");
+            console.log("Source: " + url);
+            console.log(data);
+            console.log("```");
+
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+        //https and cache in action
+        try {
+
+            const response = await axios.get('https://localhost:5657/api/json/graphical/classification/' + url);
+            const data = response.data;
+            console.log("```js");
+            console.log("Source: " + url);
+            console.log(data);
+            console.log("```");
+
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
     }
-    }
-     test3();
+    test3();
 }
 async function test1() {
     try {
@@ -69,7 +84,7 @@ async function test1() {
     }
 }
 
-async function prep(){
+async function prep() {
     await nsfwModel.init();
     const assad = require("./index.js");
     test1();
